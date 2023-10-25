cbcf108982d3c58a8a7e6ebcdfeb55f79349b1ea
Alexshwing <2722803173@qq.com>
2023-10-25 17:44:01 +0800

    feat(medium): add Diff

---
+++ src/medium/00645-Diff.ts
@@ -0,0 +1,29 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from '../test-utils'
+
+type Foo = {
+  name: string
+  age: string
+}
+type Bar = {
+  name: string
+  age: string
+  gender: number
+}
+type Coo = {
+  name: string
+  gender: number
+}
+
+type cases = [
+  Expect<Equal<Diff<Foo, Bar>, { gender: number }>>,
+  Expect<Equal<Diff<Bar, Foo>, { gender: number }>>,
+  Expect<Equal<Diff<Foo, Coo>, { age: string; gender: number }>>,
+  Expect<Equal<Diff<Coo, Foo>, { age: string; gender: number }>>,
+]
+
+// ============= Your Code Here =============
+
+type Diff<A extends Record<string, any>, B extends Record<string, any>> = {
+  [Key in Exclude<keyof A, keyof B> | Exclude<keyof B, keyof A>]: (A & B)[Key]
+}
